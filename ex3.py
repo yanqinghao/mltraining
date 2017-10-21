f9127f8ac8783951c4f2ce016932b05ff6360e89
yanqinghao <187280967@qq.com>
2017-10-22 01:27:28 +0800

    preprocess

--- ex3.py
+++ ex3.py
@@ -32,7 +32,7 @@ print "Explained variance score =", round(evs, 2)
 
 def plot_feature_importances(feature_importances, title, feature_names):
     # Normalize the importance values
-    feature_importances = 100.0 * (feature_importances /max(feature_importances))
+    feature_importances = 100.0 * (feature_importances / max(feature_importances))
     # Sort the index values and flip them so that they are arranged in decreasing order of importance
     index_sorted = np.flipud(np.argsort(feature_importances))
     # Center the location of the labels on the X-axis (for display purposes only)
@@ -44,7 +44,7 @@ def plot_feature_importances(feature_importances, title, feature_names):
     plt.ylabel('Relative Importance')
     plt.title(title)
     plt.show()
-
-plot_feature_importances(dt_regressor.feature_importances_,'Decision Tree regressor', housing_data.feature_names)
-plot_feature_importances(ab_regressor.feature_importances_,'AdaBoost regressor', housing_data.feature_names)
+if __name__ == '__main__':
+    plot_feature_importances(dt_regressor.feature_importances_,'Decision Tree regressor', housing_data.feature_names)
+    plot_feature_importances(ab_regressor.feature_importances_,'AdaBoost regressor', housing_data.feature_names)
 
--- ex4.py
+++ ex4.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.utils import shuffle
 from sklearn.ensemble import RandomForestRegressor
 from ex3 import plot_feature_importances
+from sklearn.metrics import mean_squared_error, explained_variance_score
+
 def load_dataset(filename):
     file_reader = csv.reader(open(filename, 'rb'), delimiter=',')
     X, y = [], []
@@ -14,5 +16,22 @@ def load_dataset(filename):
     feature_names = np.array(X[0])
     # Remove the first row because they are feature names
     return np.array(X[1:]).astype(np.float32), np.array(y[1:]).astype(np.float32), feature_names
+
 X, y, feature_names = load_dataset(sys.argv[1])
 X, y = shuffle(X, y, random_state=7)
+
+num_training = int(0.9 * len(X))
+X_train, y_train = X[:num_training], y[:num_training]
+X_test, y_test = X[num_training:], y[num_training:]
+
+rf_regressor = RandomForestRegressor(n_estimators=1000, max_depth=10, min_samples_split=1.0)
+rf_regressor.fit(X_train, y_train)
+
+y_pred = rf_regressor.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+evs = explained_variance_score(y_test, y_pred)
+print "\n#### Random Forest regressor performance ####"
+print "Mean squared error =", round(mse, 2)
+print "Explained variance score =", round(evs, 2)
+
+plot_feature_importances(rf_regressor.feature_importances_,'Random Forest regressor', feature_names)
