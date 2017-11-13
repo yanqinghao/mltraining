a78ba52594f08487cf704a6d511a2e18c7089fcf
yanqinghao <187280967@qq.com>
2017-11-13 18:02:01 +0800

    Opencv

--- ex41.py
+++ ex41.py
@@ -1,26 +1,34 @@
-import datetime
+# import datetime
+import requests
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.finance import quotes_historical_yahoo_ochl
+# from matplotlib.finance import quotes_historical_yahoo_ochl
 from hmmlearn.hmm import GaussianHMM
 
+params = {'tickers': 'INTC', 'begin': '1994-04-05', 'end': '2015-07-03'}
+r = requests.get('https://quantprice.herokuapp.com/api/v1.1/scoop/period', params=params)
+data = r.json()
+quotes = np.array(data["datatable"]["data"])[:,-6:]
+
 # Get quotes from Yahoo finance
-quotes = quotes_historical_yahoo_ochl("INTC",datetime.date(1994, 4, 5), datetime.date(2015, 7, 3))
+# quotes = quotes_historical_yahoo_ochl("INTC",datetime.date(1994, 4, 5), datetime.date(2015, 7, 3))
+
 # Extract the required values
-dates = np.array([quote[0] for quote in quotes], dtype=np.int)
-closing_values = np.array([quote[2] for quote in quotes])
-volume_of_shares = np.array([quote[5] for quote in quotes])[1:]
+dates = quotes[:,0]
+closing_values = quotes[:,4]
+volume_of_shares = quotes[1:,5]
 # Take diff of closing values and computing rate of change
 diff_percentage = 100.0 * np.diff(closing_values) / closing_values[:-1]
 dates = dates[1:]
 # Stack the percentage diff and volume values column-wise for training
 X = np.column_stack([diff_percentage, volume_of_shares])
+X = [x for x in X if x[0]!=None and x[1]!=None]
 # Create and train Gaussian HMM
 print "\nTraining HMM...."
 model = GaussianHMM(n_components=5, covariance_type="diag", n_iter=1000)
 model.fit(X)
 # Generate data using model
 num_samples = 500
-samples, _ = model.sample(num_samples)
-plt.plot(np.arange(num_samples), samples[:,0], c='black')
+a, samples = model.sample(num_samples)
+plt.plot(np.arange(num_samples), samples, c='black')
 plt.show()
